cc9b9eb0af7f4ef86c89b300963ba72714b6ef62
Jesper Grankvist <jesper.grankvist@hotmail.com>
2021-10-16 11:55:52 +0200

    Alla 27 produkter renderas ut

--- index.js
+++ index.js
@@ -4,100 +4,108 @@ const shoppingCart = document.getElementById("shoppingCart")
 const totalSpent = document.getElementById("totalSpent")
 
 const transactions = []
-const startValue = 0 
+const errorMessage = document.getElementById("errorMessage")
 const filterInput = document.getElementById("filterInput")
 const filterButton = document.getElementById("filterButton")
 
-filterButton.addEventListener("click", function(){
-    productContainer.innerHTML = ""
+filterButton.addEventListener("click", function () {
     const userInput = filterInput.value
-    getStoreList(userInput)
+    if (userInput < 1 || userInput > 5) {
+        errorMessage.innerText = "Välj en siffra mellan 1-5"
+    } else {
+        errorMessage.innerText = ""
+        productContainer.innerHTML = ""
+        getStoreList(userInput)
+    }
 })
 
-function createShoppingCartList(item){
+
+function createShoppingCartList(item) {
     const p = document.createElement("p")
     p.innerHTML = `${item.name} - ${item.price}`
     return p
 }
 
-function calculateTotalSpent(){
+function calculateTotalSpent() {
     let sum = 0
-    transactions.forEach(function(element){
+    transactions.forEach(function (element) {
         sum = sum + element
     })
     return sum
 }
 
-function createPriceRatingStock(item){
+function createPriceRatingStock(item) {
     const p = document.createElement("p")
-    p.innerText = `Price: ${item.price} - Rating: ${item.rating} - Stock: ${item.stock}`
+    p.innerText = `Price: ${item.price} / Rating: ${item.rating || "No rating"} / Stock: ${item.stock}`
     return p
 }
 
-function createDescription(item){
+function createDescription(item) {
     const description = document.createElement("h2")
     description.innerText = `${item.name} --- ${item.description}`
     return description
 }
 
-function createImg(item){
+function createImg(item) {
     const image = document.createElement("img")
     image.src = item.images[0].src.small
     image.alt = item.images[0].alt
     image.width = "250"
 
-    image.addEventListener("click", function(){
+    image.addEventListener("click", function () {
         transactions.push(item.price)
-        console.log(transactions)
         totalSpent.innerHTML = `Total: ${calculateTotalSpent()}`
         shoppingCart.appendChild(createShoppingCartList(item))
     })
-
     return image
 }
 
 function onBuyButton(item) {
     const button = document.createElement("button")
-    const id = item.id
-    button.innerHTML = "Buy"
-    button.value = id
+    button.innerHTML = `Köp ${item.name}`
 
     button.addEventListener("click", function () {
         transactions.push(item.price)
         totalSpent.innerHTML = `Total: ${calculateTotalSpent()}`
         shoppingCart.appendChild(createShoppingCartList(item))
-        console.log(button)
     })
     return button
 }
 
-function renderAllItems(item, userInput){
-    //console.log(item.rating)
+
+
+function renderAllItems(item, userInput) {
+
     const wrapper = document.createElement("div")
 
-    if(item.rating >= userInput){
-    wrapper.appendChild(createDescription(item))
-    wrapper.appendChild(createImg(item))
-    wrapper.appendChild(createPriceRatingStock(item))
-    wrapper.appendChild(onBuyButton(item))
+    /*
+    item.rating >= userInput || !userInput || (!item.rating && userInput <= 1
+    */
 
-    productContainer.appendChild(wrapper)
+    if (!userInput || item.rating >= userInput ) {
+
+        wrapper.appendChild(createDescription(item))
+        wrapper.appendChild(createImg(item))
+        wrapper.appendChild(createPriceRatingStock(item))
+        wrapper.appendChild(onBuyButton(item))
+
+        productContainer.appendChild(wrapper)
     }
 }
 
 
-function renderStoreItems(data, userInput){
-    data.forEach(function(item){
+function renderStoreItems(data, userInput) {
+    data.forEach(function (item) {
         renderAllItems(item, userInput)
     })
 }
 
-function getStoreList(userInput){
+function getStoreList(userInput) {
     fetch(url)
-    .then(respnse => respnse.json())
-    .then(data => {
-        renderStoreItems(data, userInput)
-    })
+        .then(respnse => respnse.json())
+        .then(data => {
+            renderStoreItems(data, userInput)
+        })
 }
 
-getStoreList(startValue)
+getStoreList()
