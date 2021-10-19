51abc8bc8ffd8640e78aa777c9e22bbbe34465ff
Jesper Grankvist <jesper.grankvist@hotmail.com>
2021-10-19 18:57:47 +0200

    Ändrar kommentarer och ordning i koden

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const url = "https://mock-data-api.firebaseio.com/webb21/products.json"
-const productContainer = document.getElementById("productContainer")
+const productContainer = document.getElementById("productContainer") // 
 const shoppingCart = document.getElementById("shoppingCart")
 const totalSpent = document.getElementById("totalSpent")
 
-const transactions = []
-const errorMessage = document.getElementById("errorMessage")
+const transactions = [] // ARRAY TO STORE ITEM/PRODUCT PRICES
+const errorMessage = document.getElementById("errorMessage") 
 const filterInput = document.getElementById("filterInput")
 const filterButton = document.getElementById("filterButton")
 
+/* FILTER SYSTEM */
 filterButton.addEventListener("click", function () {
     const userInput = filterInput.value
     if (userInput < 1 || userInput > 5) {
@@ -18,34 +19,7 @@ filterButton.addEventListener("click", function () {
         getStoreList(userInput)
     }
 })
-
-
-function createShoppingCartList(item) {
-    const p = document.createElement("p")
-    p.innerHTML = `${item.name} - ${item.price} kr`
-    return p
-}
-
-function calculateTotalSpent() {
-    let sum = 0
-    transactions.forEach(function (element) {
-        sum = sum + element
-    })
-    return sum
-}
-
-function createPriceRatingStock(item) {
-    const p = document.createElement("p")
-    p.innerText = `Price: ${item.price} / Rating: ${item.rating || "No rating"} / Stock: ${item.stock}`
-    return p
-}
-
-function createDescription(item) {
-    const description = document.createElement("h2")
-    description.innerText = `${item.name} - ${item.description}`
-    return description
-}
-
+/* CREATE IMAGE FOR EACH ITEM/PRODUCT */
 function createImg(item) {
     const image = document.createElement("img")
     image.src = item.images[0].src.small
@@ -53,12 +27,14 @@ function createImg(item) {
     image.width = "300"
     image.height = "250"
 
+    /*
+    REMOVE COMMENTS TO ENABLE CLICK ON IMAGE
     image.addEventListener("click", function () {
         onBuyClick(item)
-    })
+    }) */
     return image
 }
-
+/* CREATE BUTTON FOR EACH ITEM/PRODUCT */
 function createBuyButton(item) {
     const button = document.createElement("button")
     button.innerHTML = `Köp ${item.name}`
@@ -66,22 +42,47 @@ function createBuyButton(item) {
     button.addEventListener("click", function () {
         onBuyClick(item)
     })
-
     return button
 }
-
-function createLineBreak() {
-    const lineBreak = document.createElement("hr")
-    return lineBreak
-}
-
+/* EACH ITEM/PRODUCT BOUGHT PUSHES INTO TRANSACTIONS ARRAY, SETS INNERHTML TO RETURN VALUE FROM CALCULATETOTALSPENT, SETS INNERHTML TO RETURN VALUE FROM
+CREATESHOPPINGCARTLIST  */
 function onBuyClick(item) {
     transactions.push(item.price)
     totalSpent.innerHTML = `Total: ${calculateTotalSpent()}`
     shoppingCart.appendChild(createShoppingCartList(item))
 }
-
-
+/* FUNCTION THAT LOOPS THROUGH TRANSACTIONS ARRAY AND ADDS INTO TOTALSPENT H3 */
+function calculateTotalSpent() {
+    let sum = 0
+    transactions.forEach(function (element) {
+        sum = sum + element
+    })
+    return sum
+}
+/* FUNCTION THAT RETURNS ITEM/PRODUCT NAME AND PRICE TO SHOPPINGCART DIV */
+function createShoppingCartList(item) {
+    const p = document.createElement("p")
+    p.innerHTML = `${item.name} - ${item.price} kr`
+    return p
+}
+/* CREATES THE SPECIFIC ITEM/PRODUCT: NAME AND DESCRIPTION */
+function createDescription(item) {
+    const description = document.createElement("h2")
+    description.innerText = `${item.name} - ${item.description}`
+    return description
+}
+/* CREATES THE SPECIFIC ITEM/PRODUCT: PRICE, RATING AND STOCK */
+function createPriceRatingStock(item) {
+    const p = document.createElement("p")
+    p.innerText = `Price: ${item.price} / Rating: ${item.rating || "No rating"} / Stock: ${item.stock}`
+    return p
+}
+/* CREATES HR AND RETURNS IT*/
+function createLineBreak() {
+    const lineBreak = document.createElement("hr")
+    return lineBreak
+}
+/* RENDER ALL ITEMS IF NO USERINPUT, RENDER ITEMS BASED ON THEIR RATING */
 function renderAllItems(item, userInput) {
     const wrapper = document.createElement("div")
 
@@ -96,7 +97,6 @@ function renderAllItems(item, userInput) {
         productContainer.appendChild(wrapper)
     }
 }
-
 
 function renderStoreItems(data, userInput) {
     data.forEach(function (item) {
